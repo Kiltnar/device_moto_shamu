ed90b5755c8d01ace46a99e322c8984ac1e55386
theringmaster <gzroms@hotmail.com>
2016-02-03 18:15:02 -0500

    AOKPified

--- aokp.mk
+++ aokp.mk
@@ -2,19 +2,16 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
+DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-aokp
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_shamu
+PRODUCT_NAME := aokp_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 
